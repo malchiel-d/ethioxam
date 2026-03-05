f4d8a1767cc0f74ab87c1d3981400f2dccd9183c
Malchiel <Malchiel.dawit1@gmail.com>
2026-03-05 23:45:24 +0300

    add tests directory with mmmvp.html file

---
+++ script.js
@@ -0,0 +1,44 @@
+//used local storage so that reloading does not wipe all the data
+/*
+to make the answers flexible 
+ - track answer instead of the score
+ - convert saved answer 
+ */
+-
+let currentQuestion = Number(localStorage.getItem("questionIndex")) || 0;
+let userAnswers = JSON.parse(localStorage.getItem("savedAnswers")) || {};
+
+function loadQuestion() {
+    const activeQuestion = questions[currentQuestion];
+    
+    document.getElementById("q-text").innerText = activeQuestion.question;
+    document.getElementById("btn-A").innerText = activeQuestion.options.A;
+    document.getElementById("btn-B").innerText = activeQuestion.options.B;
+    document.getElementById("btn-C").innerText = activeQuestion.options.C;
+    document.getElementById("btn-D").innerText = activeQuestion.options.D;
+}
+
+function selectOption(choice) {
+    userAnswers[currentQuestion] = choice;
+    localStorage.setItem("savedAnswers", JSON.stringify(userAnswers));
+}
+
+function nextQuestion() {
+    if (currentQuestion < questions.length - 1) {
+        currentQuestion++;
+        localStorage.setItem("questionIndex", currentQuestion);
+        loadQuestion();
+    }
+}
+
+function previousQuestion() {
+    if (currentQuestion > 0) {
+        currentQuestion--;
+        localStorage.setItem("questionIndex", currentQuestion);
+        loadQuestion();
+    }
+}
+
+function startExam() {
+    loadQuestion();
+}

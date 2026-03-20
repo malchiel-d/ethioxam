90c84ddaa82df605d8239fbaa59d4fb8118d6818
Malchiel <Malchiel.dawit1@gmail.com>
2026-03-20 23:01:42 +0300

    refactor for clarity

--- src/script.js
+++ src/script.js
@@ -36,144 +36,153 @@ const quizData = [
     correct: "a"
   }
 ];
+
 //saving user answers 
      //null,null,null
+let currentQuestionIndex = 0;
 let userAnswers = Array(quizData.length).fill(null);
-// submit button 
-//diaplaying submit btn
-function displaySubmit(){
-const submitButton = document.querySelector('.btn-submit')
-if (userAnswers.every(answer => answer !== null)) {
-    submitButton.style.display = "block";
-} else {
-    submitButton.style.display = "none";
-} 
-}
-//submit btn clicked 
-document.querySelector("#submitBtn").addEventListener("click",submitAnswer);
-function submitAnswer(){
-if (!confirm("Are you sure you want to submit? You won't be able to change your answers.")) {
-    return; 
-  }
-    
-let score = 0;
-  
-for(let i=0; i<quizData.length;i++)    
-if(userAnswers[i] === quizData[i].correct){
-score++
-}
-const percentage = Number(quizData.length? ((score * 100 / quizData.length)).toFixed(2): 0);
+
 const quizWrapper = document.querySelector('#quiz-wrapper');
 const endScreen = document.querySelector('#endScreen');
-let scoreSpan = document.querySelector('#scoreSpan');    
-const restartBtn = document.querySelector('#restartBtn');
-    
-quizWrapper.style.display = "none";
-endScreen.style.display = "block";
+const form = document.querySelector("#quizForm");
+const clearBtn = document.querySelector('#clearBtn');
+const submitButton = document.querySelector('.btn-submit');
 
-//score span
-scoreSpan.textContent = `${percentage}%`    
-if(percentage>=50){
-scoreSpan.style.color = "#4caf50";
+//start-btn
+document.querySelector('#start-btn').addEventListener("click", startExam);
+
+function startExam() {
+  let startBtn = document.querySelector('#start-btn');
+  let quizWrapper = document.querySelector('.quiz-wrapper');
+  startBtn.style.display = "none";
+  quizWrapper.style.display = "block";
+  render();
 }
-else{
-scoreSpan.style.color = "#B00020";
+
+function render() {
+  let currentQuestion = quizData[currentQuestionIndex];
+  let savedAnswer = userAnswers[currentQuestionIndex];
+
+  //clear inputs 
+  document.querySelectorAll('#quizForm input').forEach(i => i.checked = false);
+
+  //fill inputs
+  if (savedAnswer !== null) {
+    let matchedRadio = document.querySelector(`input[value="${savedAnswer}"]`);
+    if (matchedRadio) matchedRadio.checked = true;
+    displaySubmit();
+    displayClearBtn();
+  }
+
+  //adding quiz data to screen
+  let questionHtml = document.querySelector('.quiz-wrapper #questionDiv');
+  let choiceHtml = document.querySelectorAll('.option-label');
+
+  //add question
+  questionHtml.textContent = `${currentQuestionIndex + 1}. ${currentQuestion.question}`;
+
+  //add the choioces
+  Object.values(currentQuestion.choices).forEach((choice, index) => {
+    choiceHtml[index].textContent = String.fromCharCode(97 + index) + ". " + choice;
+  });
 }
+
+//next and prev btn    
+document.querySelector('#nextBtn').addEventListener("click", next);
+document.querySelector('#previousBtn').addEventListener("click", previous);
+
+function next() {
+  if (currentQuestionIndex < quizData.length - 1) {
+    currentQuestionIndex++;
+    render();
+    displayClearBtn();
+  };
 }
-//restart btn
-restartBtn.addEventListener("click",restartExam);
-function restartExam(){
-//reset
-currentQuestionIndex = 0;
-userAnswers = Array(quizData.length).fill(null);    
-//swap
-let startBtn = document.querySelector('#start-btn');    
-startBtn.style.display = "block";
-endScreen.style.display = "none";
-//submit btn
-displaySubmit()    
+
+function previous() {
+  if (currentQuestionIndex > 0) {
+    currentQuestionIndex--;
+    render();
+    displayClearBtn();
+  };
 }
 
 //add answers from the form 
-const form = document.querySelector("#quizForm");
-
 form.addEventListener("change", function(event) {
-    userAnswers[currentQuestionIndex] = event.target.value;
-    displaySubmit()
-    displayClearBtn()
+  userAnswers[currentQuestionIndex] = event.target.value;
+  displaySubmit();
+  displayClearBtn();
 });
-let currentQuestionIndex = 0;
 
-function render(){
-let currentQuestion =  quizData[currentQuestionIndex];   
-let savedAnswer = userAnswers[currentQuestionIndex]; 
-//clear inputs 
-document.querySelectorAll('#quizForm input').forEach(i => i.checked = false);
-    
-//fill inputs
-if(savedAnswer !== null){
-let matchedRadio = document.querySelector(`input[value="${savedAnswer}"]`);
-matchedRadio.checked = true;
-displaySubmit()   
-displayClearBtn()  
-}
-
-    
-//adding quiz data to screen
-let questionHtml = document.querySelector('.quiz-wrapper #questionDiv');
-let choiceHtml = document.querySelectorAll('.option-label');     
-
-//add question
-questionHtml.textContent = `${currentQuestionIndex+1}. ${currentQuestion.question}`;
-//add the choioces
-Object.values(currentQuestion.choices).forEach((choice, index) => {
-choiceHtml[index].textContent = String.fromCharCode(97 + index) + ". " + choice;
-});
-}
-
-//start-btn
-document.querySelector('#start-btn').addEventListener("click",startExam);
-function startExam(){
-let startBtn = document.querySelector('#start-btn');
-let quizWrapper = document.querySelector('.quiz-wrapper');
-startBtn.style.display = "none";
-quizWrapper.style.display = "block";
-render();
-}
-    
 //clear-button   
-const clearBtn = document.querySelector('#clearBtn')
-function displayClearBtn(){
-  if(userAnswers[currentQuestionIndex] !== null){
+function displayClearBtn() {
+  if (userAnswers[currentQuestionIndex] !== null) {
     clearBtn.style.display = "block";
-  }
-  else{
+  } else {
     clearBtn.style.display = "none";
   }
-  
 }
+
 clearBtn.onclick = e => {
-e.preventDefault();
-document.querySelectorAll('#quizForm input').forEach(i => i.checked = false);
-userAnswers[currentQuestionIndex] = null;
-displaySubmit();
-displayClearBtn()  
-};   
-    
-//next and prev btn    
-let nextBtn = document.querySelector('#nextBtn').addEventListener("click",next);
-let previousBtn = document.querySelector('#previousBtn').addEventListener("click",previous);       
-function next(){
-if(currentQuestionIndex < quizData.length-1){
-currentQuestionIndex++ ;
-render()
-displayClearBtn()  
-}; 
-}   
-function previous(){   
-if(currentQuestionIndex > 0 ){
-currentQuestionIndex-- ;
-render()
-displayClearBtn()  
+  e.preventDefault();
+  document.querySelectorAll('#quizForm input').forEach(i => i.checked = false);
+  userAnswers[currentQuestionIndex] = null;
+  displaySubmit();
+  displayClearBtn();
 };
+
+// submit button 
+//diaplaying submit btn
+function displaySubmit() {
+  if (userAnswers.every(answer => answer !== null)) {
+    submitButton.style.display = "block";
+  } else {
+    submitButton.style.display = "none";
+  }
+}
+
+//submit btn clicked 
+document.querySelector("#submitBtn").addEventListener("click", submitAnswer);
+
+function submitAnswer() {
+  if (!confirm("Are you sure you want to submit? You won't be able to change your answers.")) {
+    return;
+  }
+
+  let score = 0;
+
+  for (let i = 0; i < quizData.length; i++)
+    if (userAnswers[i] === quizData[i].correct) {
+      score++;
+    }
+
+  const percentage = Number(quizData.length ? ((score * 100 / quizData.length)).toFixed(2) : 0);
+  let scoreSpan = document.querySelector('#scoreSpan');
+
+  quizWrapper.style.display = "none";
+  endScreen.style.display = "block";
+
+  //score span
+  scoreSpan.textContent = `${percentage}%`;
+  if (percentage >= 50) {
+    scoreSpan.style.color = "#4caf50";
+  } else {
+    scoreSpan.style.color = "#B00020";
+  }
+}
+
+//restart btn
+const restartBtn = document.querySelector('#restartBtn');
+restartBtn.addEventListener("click", restartExam);
+
+function restartExam() {
+  //reset
+  currentQuestionIndex = 0;
+  userAnswers = Array(quizData.length).fill(null);
+  //swap
+  let startBtn = document.querySelector('#start-btn');
+  startBtn.style.display = "block";
+  endScreen.style.display = "none";
+  //submit btn
+  displaySubmit();
 }
